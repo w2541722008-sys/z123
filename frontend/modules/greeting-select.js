35eb7d17ec63740dd4c9a85a75e8d911ea76d1e8
李浩宇 <773682014@qq.com>
2026-04-04 07:33:44 +0800

    fix: greeting selection v7 - rewrite with createElement + direct addEventListener

--- frontend/modules/greeting-select.js
+++ frontend/modules/greeting-select.js
@@ -1,24 +1,30 @@
 var GreetingSelect = (function() {
   var _char = null;
-  var _greetings = [];
-  var _bound = false;
 
   function open(char, greetings) {
     _char = char;
-    _greetings = greetings;
 
     var listEl = document.getElementById('greeting-list');
-    listEl.innerHTML = greetings.map(function(item) {
-      return '<div class="greeting-item" data-greeting-index="' + item.index + '">' +
+    listEl.innerHTML = '';
+
+    greetings.forEach(function(item) {
+      var div = document.createElement('div');
+      div.className = 'greeting-item';
+      div.setAttribute('data-greeting-index', String(item.index));
+      div.innerHTML =
         '<div class="greeting-item-inner">' +
           '<div class="greeting-item-label">' + escapeHtml(item.label) + '</div>' +
           '<div class="greeting-item-preview">' + escapeHtml(item.preview) + '</div>' +
         '</div>' +
-        '<span class="greeting-item-arrow">›</span>' +
-      '</div>';
-    }).join('');
+        '<span class="greeting-item-arrow">›</span>';
 
-    ensureClickDelegate();
+      div.addEventListener('click', function() {
+        handleSelect(item.index);
+      });
+
+      listEl.appendChild(div);
+    });
+
     document.getElementById('greeting-select-modal').classList.add('open');
   }
 
@@ -27,55 +33,44 @@ var GreetingSelect = (function() {
     document.getElementById('greeting-select-modal').classList.remove('open');
   }
 
-  function findGreetingItem(target) {
-    var el = target;
-    while (el && el !== document) {
-      if (el.classList && el.classList.contains('greeting-item')) return el;
-      el = el.parentNode;
-    }
-    return null;
-  }
-
-  function onItemClick(e) {
-    var item = findGreetingItem(e.target);
-    if (!item) return;
-    var index = Number(item.getAttribute('data-greeting-index'));
-    if (!isNaN(index)) select(index);
-  }
-
-  function ensureClickDelegate() {
-    if (_bound) return;
-    _bound = true;
-    var listEl = document.getElementById('greeting-list');
-    if (listEl) listEl.addEventListener('click', onItemClick);
-  }
-
-  async function select(index) {
-    close();
-    if (!_char) return;
+  function handleSelect(index) {
     var char = _char;
+    close();
+
+    if (!char) {
+      if (typeof UI !== 'undefined' && UI.toast) {
+        UI.toast('\u89d2\u8272\u4fe1\u606f\u4e22\u5931\uff0c\u8bf7\u91cd\u8bd5', 'error');
+      }
+      return;
+    }
 
     if (index === 0) {
       Chat.enterChat(char);
       return;
     }
 
-    try {
-      await safeApiCall(function() {
-        return API.clearChatWithGreeting({
-          character_id: char.id,
-          greeting_index: index,
-        });
-      });
-    } catch (err) {
-      if (err.message !== '\u672a\u767b\u5f55') {
-        UI.toast('\u5207\u6362\u5267\u60c5\u7ebf\u5931\u8d25\uff1a' + err.message, 'error');
-      }
+    if (typeof safeApiCall === 'undefined') {
+      Chat.enterChat(char);
       return;
     }
 
-    Chat.enterChat(char);
+    safeApiCall(function() {
+      return API.clearChatWithGreeting({
+        character_id: char.id,
+        greeting_index: index,
+      });
+    }).then(function(result) {
+      Chat.enterChat(char);
+    }).catch(function(err) {
+      if (err && err.message && err.message !== '\u672a\u767b\u5f55') {
+        if (typeof UI !== 'undefined' && UI.toast) {
+          UI.toast('\u5207\u6362\u5931\u8d25:' + err.message, 'error');
+        }
+      } else {
+        Chat.enterChat(char);
+      }
+    });
   }
 
-  return { open: open, close: close, select: select };
+  return { open: open, close: close, select: handleSelect };
 })();
